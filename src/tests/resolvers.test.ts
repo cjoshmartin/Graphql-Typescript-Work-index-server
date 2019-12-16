f3fd2ddc250499ba2b906f753df3c3195c7d2e46
Josh <joshmartin5001@gmail.com>
2019-12-16 02:37:06 -0500

    fix(Tests): Deleted unused test

--- src/tests/resolvers.test.ts
+++ src/tests/resolvers.test.ts
@@ -1,27 +1,3 @@
 import { expect } from 'chai';
-import resolvers from '../resolvers';
 
-describe('GraphQL resolvers', () => {
-  const test = [
-    {
-      "id": "24341d42-8235-47a1-9ec5-c6afcbdcef16",
-      "firstName": "Ofelia",
-      "lastName": "Buckridge",
-      "jobTitle": "Direct Applications Architect",
-      "departmentId": "cfd90465-28fa-4b9a-be3e-ef2517e987e9",
-      "managerId": "d44390cd-b306-4e11-b7d5-a5e0e6fe1e3d"
-    },
-    {
-      "id": "d44390cd-b306-4e11-b7d5-a5e0e6fe1e3d",
-      "firstName": "Asia",
-      "lastName": "Streich",
-      "jobTitle": "Dynamic Branding Orchestrator",
-      "departmentId": "aef293ee-8dcc-4d89-99cf-1b8f61bab07b",
-      "managerId": "2798c35b-5b8f-4a5d-9858-0a818d48cbef"
-    },
-    
-  ];
-  describe('people', () => {
-
-  });
-});
+// Resolvers get tested in Datasource test as well as in Muations test
